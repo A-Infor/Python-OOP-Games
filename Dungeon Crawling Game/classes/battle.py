d807700082fd753d8278ab6cb317ad2c061f0682
A-Infor <anderson.informatica.mg@gmx.com>
2025-01-30 22:49:25 -0300

    Incremented  class. Now there is a very simple working battle system in the game.

--- Dungeon Crawling Game/classes/battle.py
+++ Dungeon Crawling Game/classes/battle.py
@@ -4,13 +4,48 @@ class Battle:
     def __init__(self, player, enemy):
         self.player = player
         self.enemy  = enemy
+        
+        self.play_battle()
     
-    def _choose_starting_character(self):
+    def play_battle(self):        
+        for round_counter in range(1, 1000, 1):
+            if self.enemy.get_stats()['health'] > 0 and self.player.get_stats()['health'] > 0:
+                round_winner   = self._play_round(round_counter)
+                round_counter += 1
+                print(f'Round winner: {round_winner}')
+            elif self.enemy.get_stats()['health'] == 0 or self.player.get_stats()['health'] == 0:
+                battle_winner = self._define_winner()
+                print(f'\nBattle winner: {battle_winner}')
+                return
+        print('\nThe battle becomes too long. The rivals can\'t keep battling anymore, and fall in the ground, exhausted.')
+        
+    def _play_round(self, round_number):
+        (attacker, defender) = self._define_attacker_and_defender()
+        attacker_attack      = attacker.get_stats()['attack']
+        defender_defense     = defender.get_stats()['defense']
+        damage_caused        = max(0, attacker_attack - defender_defense)
+        
+        print(f'\nRound {round_number} started!')
+        print(f"\t{attacker.get_stats()['NAME']} attacks with {attacker_attack} points of force")
+        print(f"\t{defender.get_stats()['NAME']} blocks the attack by {defender_defense} points")
+        
+        print(f'\tThe resulting damage is {damage_caused}')
+        defender.stat_decrease('health', damage_caused)
+        
+        print( '\t      PLAYER\tENEMY')
+        print(f"\tLIFE:   {self.player.get_stats()['health']}    \t  {self.enemy.get_stats()['health']}")
+        
+        return self._define_winner()
+        
+    def _define_attacker_and_defender(self):
         starting_character = random.randint(0, 1)
-        return 'player' if starting_character == 0 else 'enemy'
+        return (self.player, self.enemy) if starting_character == 0 else (self.enemy, self.player)
     
-    def play_battle(self):
-        self._play_round()
-    
-    def _play_round(self):
-        self._choose_starting_character()
+    def _define_winner(self):
+        if   0 == self.player.get_stats()['health'] == self.enemy.get_stats()['health']:
+            return None
+        elif self.player.get_stats()['health'] > self.enemy.get_stats()['health']:
+            return 'PLAYER'
+        elif self.player.get_stats()['health'] < self.enemy.get_stats()['health']:
+            return 'ENEMY'
+        
--- Dungeon Crawling Game/classes/character.py
+++ Dungeon Crawling Game/classes/character.py
@@ -1,27 +1,33 @@
 import random
 
 class Character:
-    def __init__(self, is_human = False):
+    def __init__(self, name, is_human = False):
+        self._name     = name.upper()
         self._is_human = is_human
         self._level    = 1
         self._health   = random.randint(1, 9)
         self._attack   = random.randint(1, 9)
         self._defense  = random.randint(1, 9)
-        
-        self.print_stats()
-        
-    def print_stats(self):
-        print('\nPLAYER' if self._is_human == True else '\nENEMY')
-        print(f'LIFE: {self._health}')
-        print(f'LVL : {self._level}')
-        print(f'ATK : {self._attack}')
-        print(f'DEF : {self._defense}')
     
-    def stat_decrease(self, stat, how_much):
-        self._health -= how_much
+    def get_stats(self):
+        return {'NAME'    : self._name    ,
+                'health'  : self._health  ,
+                'level'   : self._level   ,
+                'attack'  : self._attack  ,
+                'defense' : self._defense
+                }
     
-    def stat_increase(self, stat, how_much):
-        self._health += how_much
+    def stat_decrease(self, stat_name, how_much):
+        if how_much > 0:
+            stat_value = getattr(self, '_' + stat_name)
+            stat_value -= how_much
+            setattr(self, '_' + stat_name, stat_value if stat_value >= 0 else 0)
+    
+    def stat_increase(self, stat_name, how_much):
+        if how_much > 0:
+            stat_value = getattr(self, '_' + stat_name)
+            stat_value += how_much
+            setattr(self, '_' + stat_name, stat_value)
     
     def _level_up(self):
         pass
